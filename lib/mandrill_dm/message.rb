3028a5d69f3ad2b5879c9028ab3f6984cd2ca03d
Pete Brooks <pete.brooks@tablexi.com>
2018-03-13 15:21:29 -0500

    Add workaround for object arrays in merge vars

--- lib/mandrill_dm/message.rb
+++ lib/mandrill_dm/message.rb
@@ -53,7 +53,14 @@ module MandrillDm
     end
 
     def global_merge_vars
-      get_value(:global_merge_vars)
+      value = get_value(:global_merge_vars)
+      return value unless value.is_a?(String)
+      # When given a nested array, the value returned by the Mail instance
+      # may be converted to an invalid Ruby string. To avoid the security issues
+      # of using `eval`, a workaround is to set `global_merge_vars` to a JSON
+      # string when sending the message. When it is read here, the string will
+      # have newlines and carriage returns inserted, for reasons that remain unclear.
+      JSON.parse(value.gsub(/[\r|\n]/, ""))
     end
 
     def headers
